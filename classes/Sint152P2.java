f781783b8d1dd59d40e74e4bb538663dcb762cb2
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-11-02 18:07:07 +0100

    Home button

--- classes/Sint152P2.java
+++ classes/Sint152P2.java
@@ -9,7 +9,7 @@ public class Sint152P2 extends HttpServlet {
 		Map<String, Integer> vars = createMap(req);
 		printPage(res, vars);
 	}
-	
+
 	Map<String, Integer> createMap(HttpServletRequest req){
 		int consulta, fase, interprete, album, año, estilo;
 		consulta = fase = interprete = album = año = estilo = 0;
@@ -21,7 +21,7 @@ public class Sint152P2 extends HttpServlet {
 				interprete = Integer.parseInt(req.getParameter("interprete"));
 				album = Integer.parseInt(req.getParameter("album"));
 				año = Integer.parseInt(req.getParameter("año"));
-				estilo = Integer.parseInt(req.getParameter("estilo"));	
+				estilo = Integer.parseInt(req.getParameter("estilo"));
 			}
 		}catch(NumberFormatException e){
 				System.out.println("Error de parseo de int");
@@ -34,7 +34,7 @@ public class Sint152P2 extends HttpServlet {
 		vars.put("estilo", estilo);
 		return vars;
 	}
-	
+
 	void printPage(HttpServletResponse res, Map vars) throws IOException {
 		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
@@ -55,64 +55,92 @@ public class Sint152P2 extends HttpServlet {
 		out.println("</body>");
 		out.println("</html>");
 	}
-	
+
 	void printForm(PrintWriter out, Map vars){
-		if((int)vars.get("consulta")==1){
-			if((int)vars.get("fase")==1){
+		if((Integer)vars.get("consulta")==1){
+			if((Integer)vars.get("fase")==1){
 				out.println("<h2>Selecciona un intérprete:</h2>");
 				out.println("<form>");
 				out.println("<input type=\"radio\" name=\"interprete\" value=1 checked>Intérprete 1<br>");
 				out.println("<input type=\"radio\" name=\"interprete\" value=2>Intérprete 2<br>");
 				out.println("<input type=\"radio\" name=\"interprete\" value=3>Intérprete 3<br>");
 				out.println("<input type=\"radio\" name=\"interprete\" value=4>Intérprete 4<br>");
+				out.println("<input type=\"hidden\" name=\"consulta\" value=1><br>");
 				out.println("<input type=\"hidden\" name=\"fase\" value=2><br>");
+				out.println("<input type=\"submit\" value=\"enviar\">");
 			}
-			if((int)vars.get("fase")==2){
+			if((Integer)vars.get("fase")==2){
 				out.println("<h2>Selecciona un álbum:</h2>");
 				out.println("<form>");
 				out.println("<input type=\"radio\" name=\"album\" value=1 checked>Álbum 1<br>");
 				out.println("<input type=\"radio\" name=\"album\" value=2>Álbum 2<br>");
 				out.println("<input type=\"radio\" name=\"album\" value=3>Álbum 3<br>");
+				out.println("<input type=\"radio\" name=\"album\" value=4>Álbum 4<br>");
+				out.println("<input type=\"hidden\" name=\"consulta\" value=1><br>");
 				out.println("<input type=\"hidden\" name=\"fase\" value=3><br>");
+				out.println("<input type=\"submit\" value=\"inicio\" onclick=\"form.consulta.value=0\">");
+				out.println("<input type=\"submit\" value=\"enviar\">");
 			}
-		}else if((int)vars.get("consulta")==2){
-			if((int)vars.get("fase")==1){
+			if((Integer)vars.get("fase")==3){
+				out.println("<h2>Listado de canciones especificadas:</h2>");
+				out.println("<form>");
+				out.println("<li>Canción 1 (duración, descripción)</li>");
+				out.println("<li>Canción 2 (duración, descripción)</li>");
+				out.println("<li>Canción 3 (duración, descripción)</li>");
+				out.println("<li>Canción 4 (duración, descripción)</li><br>");
+				out.println("<input type=\"hidden\" name=\"consulta\" value=1><br>");
+				out.println("<input type=\"submit\" value=\"inicio\" onclick=\"form.consulta.value=0\">");
+			}
+		}else if((Integer)vars.get("consulta")==2){
+			if((Integer)vars.get("fase")==1){
 				out.println("<h2>Selecciona un año:</h2>");
 				out.println("<form>");
 				out.println("<input type=\"radio\" name=\"año\" value=1 checked>Año 1<br>");
 				out.println("<input type=\"radio\" name=\"año\" value=2>Año 2<br>");
 				out.println("<input type=\"radio\" name=\"año\" value=3>Año 3<br>");
 				out.println("<input type=\"radio\" name=\"año\" value=4>Año 4<br>");
+				out.println("<input type=\"hidden\" name=\"consulta\" value=2><br>");
 				out.println("<input type=\"hidden\" name=\"fase\" value=2><br>");
+				out.println("<input type=\"submit\" value=\"enviar\">");
 			}
-			if((int)vars.get("fase")==2){
+			if((Integer)vars.get("fase")==2){
 				out.println("<h2>Selecciona un álbum:</h2>");
 				out.println("<form>");
 				out.println("<input type=\"radio\" name=\"album\" value=1 checked>Álbum 1<br>");
 				out.println("<input type=\"radio\" name=\"album\" value=2>Álbum 2<br>");
 				out.println("<input type=\"radio\" name=\"album\" value=3>Álbum 3<br>");
-				out.println("<input type=\"radio\" name=\"album\" value=4>Álbum 4<br>");	
-				out.println("<input type=\"hidden\" name=\"fase\" value=3><br>");			
+				out.println("<input type=\"radio\" name=\"album\" value=4>Álbum 4<br>");
+				out.println("<input type=\"hidden\" name=\"consulta\" value=2><br>");
+				out.println("<input type=\"hidden\" name=\"fase\" value=3><br>");
+				out.println("<input type=\"submit\" value=\"inicio\" onclick=\"form.consulta.value=0\">");
+				out.println("<input type=\"submit\" value=\"enviar\">");
 			}
-			if((int)vars.get("fase")==3){
+			if((Integer)vars.get("fase")==3){
 				out.println("<h2>Selecciona un estilo:</h2>");
 				out.println("<form>");
 				out.println("<input type=\"radio\" name=\"estilo\" value=1 checked>Estilo 1<br>");
 				out.println("<input type=\"radio\" name=\"estilo\" value=2>Estilo 2<br>");
 				out.println("<input type=\"radio\" name=\"estilo\" value=3>Estilo 3<br>");
 				out.println("<input type=\"radio\" name=\"estilo\" value=4>Estilo 4<br>");
+				out.println("<input type=\"hidden\" name=\"consulta\" value=2><br>");
 				out.println("<input type=\"hidden\" name=\"fase\" value=4><br>");
+				out.println("<input type=\"submit\" value=\"inicio\" onclick=\"form.consulta.value=0\">");
+				out.println("<input type=\"submit\" value=\"enviar\">");
 			}
-			if((int)vars.get("fase")==3){
-			}	
-		}else if((int)vars.get("consulta")==0){
+			if((Integer)vars.get("fase")==4){
+				out.println("<h2>El número de canciones es: 4</h2>");
+				out.println("<form>");
+				out.println("<input type=\"hidden\" name=\"consulta\" value=2><br>");
+				out.println("<input type=\"submit\" value=\"inicio\" onclick=\"form.consulta.value=0\">");
+			}
+		}else if((Integer)vars.get("consulta")==0){
 			out.println("<h2>Selecciona una consulta:</h2>");
 			out.println("<form>");
 			out.println("<input type=\"radio\" name=\"consulta\" value=1 checked>Lista de canciones de un álbum<br>");
 			out.println("<input type=\"radio\" name=\"consulta\" value=2>Número de canciones de un álbum<br>");
 			out.println("<input type=\"hidden\" name=\"fase\" value=1><br>");
+			out.println("<input type=\"submit\" value=\"enviar\">");
 		}
-		out.println("<input type=\"submit\" value=\"enviar\">");
 		out.println("</form>");
 	}
 }
