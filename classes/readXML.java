8556944c36f1fb2ca3626b4974815c1cc84c06bb
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-11-22 18:08:09 +0100

    Parse all XMLs and print some nodes info

--- classes/readXML.java
+++ classes/readXML.java
@@ -10,22 +10,51 @@ import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.io.IOException;
+import java.io.File;
 
 import javax.xml.xpath.*;
 
 public class readXML {
-	public static void main (String args[]){
+	public static void main (String args[]) throws XPathExpressionException{
+		Document doc = null;
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		dbf.setValidating(true);
-		try{
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			db.setErrorHandler(new XML_DTD_ErrorHandler());
-			Document doc = db.parse("/home/jacoboqc/public_html/webapps/WEB-INF/radiohead.xml");
-			System.out.println("El tipo de documento es: " + doc.getDoctype().getName());
+		Document[] docs = parseXML(dbf);
+		XPath xpath= XPathFactory.newInstance().newXPath();
+		for(int i = 0; i<3; i++){
+			System.out.println(xpath.evaluate("/Interprete/Nombre/NombreG", docs[i].getDocumentElement()));
+		}
+	}
+	
+	static Document[] parseXML (DocumentBuilderFactory dbf){
+		Document doc = null;
+		Document docs[] = new Document[3];
+		String path = "/home/jacoboqc/public_html/webapps/WEB-INF/radiohead.xml";
+		String nextIML = "first";
+		int i = 0;
+		while(!nextIML.isEmpty()){
+			try{
+				DocumentBuilder db = dbf.newDocumentBuilder();
+				db.setErrorHandler(new XML_DTD_ErrorHandler());
+				doc = db.parse(path);
+			}catch(ParserConfigurationException | SAXException | IOException e){
+				e.printStackTrace();
+			}
 			XPath xpath= XPathFactory.newInstance().newXPath();
-		}catch(ParserConfigurationException | SAXException | IOException e){
-			e.printStackTrace();
-		}	
+			Element raiz = doc.getDocumentElement();
+			try{
+				System.out.println("El nombre del elemento actual es: " + xpath.evaluate("/Interprete/Nombre/NombreG", raiz));
+				nextIML = xpath.evaluate("//IML", raiz);
+			}catch(XPathExpressionException e){
+				e.printStackTrace();
+			}
+			path = path.replaceFirst(new File(path).getName(), nextIML);
+			System.out.println("El IML del siguiente elemento es: " + nextIML);
+			System.out.println("El path del siguiente elemento es: " + path);
+			docs[i] = doc;
+			i++;
+		}
+		return docs;
 	}
 }
 
